041fc3e5621dff558b02bb2f3c063180e73a90cf
zhou yu <524112470@qq.com>
2020-05-03 23:11:25 +0800

    do string

---
+++ BaseAlgorithm/pString3.py
@@ -0,0 +1,27 @@
+"""
+字符串中的第一个唯一字符
+给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+案例:
+
+s = "leetcode"
+返回 0.
+
+s = "loveleetcode",
+返回 2.
+
+
+注意事项：您可以假定该字符串只包含小写字母。
+"""
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for i in range(len(s)):
+            j = s.count(s[i])
+            if j == 1:
+                return i
+
+
+        return -1
+

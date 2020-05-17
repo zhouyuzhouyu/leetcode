a11cb7e43841daa1f5a45609bfd5fdc0d8c742b7
zhou yu <524112470@qq.com>
2020-05-18 02:04:17 +0800

    do 55

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-str1 = "132"
-print(sorted(str1))
-
-
-
-class tt:
-    def __init__(self, str1:str):
-        self.str1 = str1
-
-
-bb = map(tt,["1","2"])
-
-print(bb)
+print('\033[1;5;33;44;4mHello, World!\033[0m')

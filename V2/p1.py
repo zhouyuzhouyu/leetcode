c6a89578795def82a107faf44305448d6867655c
zhou yu <524112470@qq.com>
2022-01-29 08:46:43 +0800

    V2

---
+++ V2/p1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Solution:
+    def twoSum(self, nums: [int], target: int) -> [int]:
+        hashMap = {}
+        for index, num in enumerate(nums):
+            if hashMap.get(target - num) is not None:
+                return [hashMap.get(target - num), index]
+            else:
+                hashMap[num] = index
+

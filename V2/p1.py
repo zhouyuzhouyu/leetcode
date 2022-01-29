f21327b11546ed068e59bcf446ee8f1d9a7c7fff
zhou yu <524112470@qq.com>
2022-01-29 10:54:37 +0800

    test6

--- V2/p1.py
+++ V2/p1.py
@@ -3,6 +3,8 @@
 class Solution:
     def twoSum(self, nums: [int], target: int) -> [int]:
         hashMap = {}
+
+        s = 6
         for index, num in enumerate(nums):
             if hashMap.get(target - num) is not None:
                 return [hashMap.get(target - num), index]
